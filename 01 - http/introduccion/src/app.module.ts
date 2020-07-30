1a44f5de808ecedc72259994bdcbd7af837601f5
Rafael Vinueza <rafaelvinueza@gmail.com>
2020-07-30 14:15:58 -0500

    Añadida la funcionalidad del examen a deber 1 y los archivos de usuario vistos en clase

--- 01 - http/introduccion/src/app.module.ts
+++ 01 - http/introduccion/src/app.module.ts
@@ -2,13 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {HttpJuegoModule} from "./http/http-juego.module";
-import {Deber1Module} from "./Deber1/deber1.module";
+import {Deber1Module} from "./Deber1_Examen/deber1.module";
+import {UsuarioController} from "./usuario/usuario.controller";
+import {UsuarioModule} from "./usuario/usuario.module";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario/usuario.entity";
 
 @Module({
   imports: [
       //Aqui van los otros modulos
       HttpJuegoModule,
-      Deber1Module
+      UsuarioModule,
+      Deber1Module,
+      TypeOrmModule.forRoot({
+          name:'default', //nombre conexion
+          type: 'mysql', //mysql, postgres
+          host: 'localhost', //ip
+          port: 3306, //puerto
+          username: 'root', //usuario
+          password: '123456789', //password
+          database: 'test', //base de datos
+          entities: [
+              //todas las entidades
+              UsuarioEntity
+          ],
+          synchronize: true, //actualiza el esquema de la base de datos
+          dropSchema: false, //Eliminar datos y el esquema de la base de datos
+      })
+
   ],
   controllers: [
       //Controladoes del app module
